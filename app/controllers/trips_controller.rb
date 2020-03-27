544cc27a5a34044990b5f093e74614c831bbf1c8
gigi <mmgamao1@gmail.com>
2020-03-27 15:37:48 -0700

    fixed trip controller and views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,15 @@ class TripsController < ApplicationController
       @trip = Trip.new
     end
   
+    def create
+        @trip = Trip.new(trip_params).merge(user_id: @current_user.id)
+        if @trip.save
+        redirect_to trip_path(@trip)
+        else
+          render :new    
+        end
+    end
+
     def index
       @trips = Trip.all
     end
@@ -12,4 +21,16 @@ class TripsController < ApplicationController
   
     def show
     end
+
+      
+  private
+  
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name, :start_date, :end_date, :user_id)
+  end
 end
+

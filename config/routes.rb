4f4a2848d89f5e0eb9151e8b85a995e99a32ccea
gigi <mmgamao1@gmail.com>
2020-03-28 17:23:26 -0700

    views edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks'}
+
   root to: "home#index"
   resources :trips
   resources :destinations
